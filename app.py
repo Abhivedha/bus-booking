b90b74d4d9880f3205741a58056a4a87f08c1a90
A <aa909@apac.comcast.com>
2025-04-17 12:47:41 +0530

    Changes in home page styling

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 db = pymysql.connect(
     host="127.0.0.1",       
     user="root",
-    password="tamilRAJAN@01",
+    password="01022004",
     database="bus_booking"  
 )
 
